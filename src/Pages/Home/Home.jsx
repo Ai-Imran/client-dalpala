d89f86a112fcdf168ebf645ccff5edd6e4756a29
unknown <Ai-Imran>
2024-04-16 23:52:07 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2>homw</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Shared/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2>nam</h2>
+        </div>
+    );
+};
+
+export default Navbar;
