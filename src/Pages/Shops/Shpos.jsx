adf61c5f8ad160b095c94302072b5942870795bd
unknown <Ai-Imran>
2024-04-18 08:13:26 +0600

    add product.json

---
+++ src/Pages/Shops/Shpos.jsx
@@ -0,0 +1,10 @@
+
+const Shpos = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default Shpos;
--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -6,11 +6,15 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Signup from "../Pages/Signup/Signup";
+import Contact from "../Pages/Contact/Contact";
+import Shpos from "../Pages/Shops/Shpos";
+import error from '../assets/error.png'
   
   export const router = createBrowserRouter([
     {
         path: "/",
         element: <Main/>,
+        errorElement: <img className="mx-auto lg:mt-[100px] mt-[120px] lg:w-[300px]" src={error} alt="" /> ,
         children : [
             {
                 path: '/',
@@ -23,6 +27,14 @@ import Signup from "../Pages/Signup/Signup";
             {
               path: 'signup',
               element:<Signup/>
+            },
+            {
+              path: 'contact',
+              element: <Contact/>
+            },
+            {
+              path: 'shops',
+              element : <Shpos/>
             }
             
         ]
